c989edfa8864ead1c1ab2ea9efb19f3c2077e3c1
SOPHIA STILES <sophia6179@gmail.com>
2025-04-27 04:06:51 -0700

    Add PDF file raeder

--- file_reader.py
+++ file_reader.py
@@ -2,6 +2,10 @@ import csv
 import json
 import io
 import email
+from pdf2image import convert_from_path
+import pytesseract
+import numpy as np
+import cv2
 
 
 def read_csv(file):
@@ -9,8 +13,26 @@ def read_csv(file):
     reader = csv.DictReader(io.StringIO(file.read().decode('utf-8')))
     return json.dumps(list(reader))
 
+def preprocess_image(pil_image):
+    img = np.array(pil_image)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    return img
+
 def read_pdf(file):
-    return ""
+    """ Given a PDF file, extract its text using OCR and return the text as a string."""
+    
+    file_path = "/tmp/temp_pdf.pdf"
+    with open(file_path, 'wb') as f:
+        f.write(file.read())
+    images = convert_from_path(file_path, dpi=300)
+    full_text = ""
+    custom_config = r'--oem 3 --psm 6'
+    for i, img in enumerate(images):
+        preprocessed_img = preprocess_image(img)
+        text = pytesseract.image_to_string(preprocessed_img, config=custom_config)
+        text = text.replace('$', 'S')
+        full_text += text
+    return json.dumps({"extracted_text": full_text})
 
 def read_txt(file):
     file.seek(0)
